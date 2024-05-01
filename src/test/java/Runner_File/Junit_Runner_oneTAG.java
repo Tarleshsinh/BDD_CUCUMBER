156ff962ba7fdcfa25bc4ba600261aa284127e92
Tarleshsin <tarlesh_parmar@yahoo.com>
2024-05-01 18:07:56 -0400

    Commited after Upgraded to Java17

--- src/test/java/Runner_File/Junit_Runner_oneTAG.java
+++ src/test/java/Runner_File/Junit_Runner_oneTAG.java
@@ -7,21 +7,15 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	
-		dryRun=false,
-		monochrome=true,
-		features= {"src/test/resources/Cucumber"},
-		glue= {"StepDefinition_Files"},
-		plugin={"pretty","html:target/site/cucumber-html",
-				"json:target/cucumber1.json"},
-		tags= "@Dropdown1"
-		
-		)
-
-
+    dryRun=false,
+    monochrome=true,
+    features= {"src/test/resources/Cucumber"},
+    glue= {"StepDefinition_Files"},
+    plugin={"pretty","html:target/site/cucumber-html", "json:target/cucumber1.json"},
+    tags= "@Login",
+    publish = true // Activates publishing of Cucumber reports
+)
 
 public class Junit_Runner_oneTAG {
-
-	
-	
+    
 }
